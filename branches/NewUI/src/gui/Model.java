e3744b7284f07871f7a3e93f2102eaebfd09caf9
zdac085 <zdac085@c378c551-b4bd-4fd4-84fa-af6bf24c0965>
2017-11-28 23:10:38 +0000

    Added JavaDoc to the rest of the classes
    git-svn-id: https://svn.cim.rhul.ac.uk/personal/zdac085/Calculator@34 c378c551-b4bd-4fd4-84fa-af6bf24c0965

--- branches/NewUI/src/gui/Model.java
+++ branches/NewUI/src/gui/Model.java
@@ -1,7 +1,17 @@
 package gui;
 
+/**
+ * This class will be used to communicate the calculation to the controller.
+ * Code adapted from code supplied by Dave Cohen.
+ *
+ * @author Marcus Messer
+ */
 public class Model {
 
+  /**
+   * Temp method that returns the answer.
+   * @return The answer to the calculation.
+   */
   public String getAns() {
     return "1234556";
   }
