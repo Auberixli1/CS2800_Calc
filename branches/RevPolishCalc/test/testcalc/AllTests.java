f0d21f32c651dcde7368a300677c9c22ce5062ab
zdac085 <zdac085@c378c551-b4bd-4fd4-84fa-af6bf24c0965>
2017-11-29 00:31:16 +0000

    Created new branch to work on the reverse polish calculator.
    git-svn-id: https://svn.cim.rhul.ac.uk/personal/zdac085/Calculator@36 c378c551-b4bd-4fd4-84fa-af6bf24c0965

---
+++ branches/RevPolishCalc/test/testcalc/AllTests.java
@@ -0,0 +1,10 @@
+package testcalc;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestEntry.class, TestStack.class})
+public class AllTests {
+
+}
