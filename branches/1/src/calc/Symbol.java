839b2b620f389514eed56be865867337383ce2b3
zdac085 <zdac085@c378c551-b4bd-4fd4-84fa-af6bf24c0965>
2017-12-06 14:00:50 +0000

    Cut a tag for the working GUI
    git-svn-id: https://svn.cim.rhul.ac.uk/personal/zdac085/Calculator@50 c378c551-b4bd-4fd4-84fa-af6bf24c0965

---
+++ branches/1/src/calc/Symbol.java
@@ -0,0 +1,39 @@
+package calc;
+
+/**
+ * This is the list of possible symbols.
+ *
+ * @author Marcus Messer
+ */
+public enum Symbol {
+  /**
+   * The enumeration for '('.
+   */
+  LEFT_BRACKET,
+
+  /**
+   * The enumeration for ')'.
+   */
+  RIGHT_BRACKET,
+  /**
+   * The enumeration for '*'.
+   */
+  TIMES,
+  /**
+   * The enumeration for '/'.
+   */
+  DIVIDE,
+  /**
+   * The enumeration for '+'.
+   */
+  PLUS,
+  /**
+   * The enumeration for '-'.
+   */
+  MINUS,
+  /**
+   * The enumeration for and INVALID symbol.
+   */
+  INVALID
+
+}
